241aab6768e20ae318144204c1549c7352a6d161
WWW_cool <WWW_cool@inbox.ru>
2020-11-16 14:49:31 +0300

    fixed compose

--- docker-compose.sh
+++ docker-compose.sh
@@ -10,8 +10,5 @@ services:
       - $HOME/.cache:/home/$UNAME/.cache
     working_dir: $PWD
     command: /sbin/init
-    depends_on:
-      bender:
-        condition: service_healthy
     mem_limit: 256M
 EOF
